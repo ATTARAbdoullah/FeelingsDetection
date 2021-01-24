682e3e5fa7dfef0f856516a8e9031922e94f3578
ATTARAbdoullah <72859285+ATTARAbdoullah@users.noreply.github.com>
2021-01-24 23:06:51 +0100

    installing pandas

--- Twitter.py
+++ Twitter.py
@@ -1,5 +1,6 @@
 import tweepy
 import credentials 
+import pandas as pd
 
 def api():
     # Create the authentication object
@@ -20,25 +21,61 @@ def timeline(n):
     for tweet in public_tweets:
         print(tweet.text + "\n")
 
-def searchQuery(q):
+def searchQuery(q,lang):
     #Search querry q on 7-day limit timeline of recent and popular( result_type = mixed ) tweets 
-    return api().search(q)
-    
-def writeSearchedQuery(q):
+    searchedQuery = api().search(q,lang=lang)
+    for tweet in searchedQuery:
+        print(tweet.text+ "\n")
+    print("Nombre de tweets: " + str(len(searchedQuery)))
+   
+def writeSearchedQuery(q,lang):
     #Looking for querry q and write it down on an f file
     f= open("output.txt","w+", encoding="utf-8")
-    tweets=searchQuery(q)
-    f.write(str(tweets))
+    tweets= api().search(q,lang=lang)
+    for tweet in tweets:
+        f.write(str(tweet.text) + "\n")
     f.close()   
 
+def temp():
+    columns = set()
+    allowed_types = [str, int]
+    tweets_data = []
+    my_timeline=api().home_timeline()
+    
+    for status in my_timeline:
+        #print(status.author.screen_name)
+        # print(type(vars(status)))
+        status_dict = dict(vars(status))
+        keys = status_dict.keys()
+        single_tweet_data = {"author": status.author.screen_name}
+        for k in keys:
+            try:
+                v_type = type(status_dict[k])
+            except:
+                v_type = None
+            if v_type != None:
+                if v_type in allowed_types:
+                    single_tweet_data[k] = status_dict[k]
+                    columns.add(k)
+        tweets_data.append(single_tweet_data)
+     
+    header_cols = list(columns)
+    header_cols.append('author')
+    df = pd.DataFrame(tweets_data, columns=header_cols)
+    f= open("output.txt","w+", encoding="utf-8")
+    f.write(str(df))
+    f.close() 
+    return df
+
 def main():
-    api()
-    timeline(2)
-    #print(searchQuery("#MAR"))
-    writeSearchedQuery("#MAR")
+    #temp()
+    #timeline(2) #Home Page
+    searchQuery("#France","fr")
+    writeSearchedQuery("#France","fr")
     
 
 if __name__ == "__main__":
    main()
+   
 
   
