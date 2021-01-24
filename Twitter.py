f90be6e6a271e8f4fba2cc946c9b1e1620fb449c
ATTARAbdoullah <72859285+ATTARAbdoullah@users.noreply.github.com>
2021-01-24 15:23:42 +0100

    Refactoring

--- Twitter.py
+++ Twitter.py
@@ -1,18 +1,44 @@
 import tweepy
 import credentials 
 
-# Create the authentication object
-authenticate = tweepy.OAuthHandler(credentials.consumerKey, credentials.consumerSecret) 
+def api():
+    # Create the authentication object
+    authenticate = tweepy.OAuthHandler(credentials.consumerKey, credentials.consumerSecret) 
 
-# Set the access token and access token secret
-authenticate.set_access_token(credentials.accessToken, credentials.accessTokenSecret) 
+    # Set the access token and access token secret
+    authenticate.set_access_token(credentials.accessToken, credentials.accessTokenSecret) 
 
-# Creating the API object while passing in auth information
-api = tweepy.API(authenticate)
+    # Creating the API object while passing in auth information
+    api = tweepy.API(authenticate)
 
-print('Welcome to your timeline \n')
+    return api
 
-#print the last 2 tweets in my timeline
-public_tweets = api.home_timeline(count=2)
-for tweet in public_tweets:
-    print(tweet.text + "\n")
+def timeline(n):
+    #print the last n tweets in the timeline
+    print('Welcome to your timeline \n')
+    public_tweets = api().home_timeline(count=n)
+    for tweet in public_tweets:
+        print(tweet.text + "\n")
+
+def searchQuery(q):
+    #Search querry q on 7-day limit timeline of recent and popular( result_type = mixed ) tweets 
+    return api().search(q)
+    
+def writeSearchedQuery(q):
+    #Looking for querry q and write it down on an f file
+    f= open("output.txt","w+", encoding="utf-8")
+    tweets=searchQuery(q)
+    f.write(str(tweets))
+    f.close()   
+
+def main():
+    api()
+    timeline(2)
+    #print(searchQuery("#MAR"))
+    writeSearchedQuery("#MAR")
+    
+
+if __name__ == "__main__":
+   main()
+
+  
